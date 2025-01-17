f17bf1c5137527f28c2618244292527790b25d11
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-01-17 23:29:44 +0530

    adding peering connection value as false

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,8 @@
 # output "azs_info" { #catching availability zones from the module-vpc
 #   value       = module.vpc.azs
 # }
+
+#displaying and catching outputs of the module.vpc 
  output "vpc_id" {
    value = module.vpc.vpc_id
  }
